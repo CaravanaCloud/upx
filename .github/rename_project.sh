6c74d6ffabdc2744511cff92122de348dd3f55d8
faermanj <faermanj@users.noreply.github.com>
2024-05-29 11:57:59 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="CaravanaCloud"
+original_name="upx"
+original_urlname="upx"
+original_description="Awesome upx created by CaravanaCloud"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv upx $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ upx/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for upx."""
+
+from upx.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
